0a2d2e8f96712803a2c1050bd4749d5e211b2303
ceasar28 <emmanuelugochukwuekete@gmail.com>
2025-02-09 14:15:40 +0100

    port

--- src/xbot/markups/followers.ts
+++ src/xbot/markups/followers.ts
@@ -1,6 +1,6 @@
 export const followersMarkUp = async (followers: any) => {
   return {
-    message: `<b>Follwers with 90k and above followers</b>:\n\n${followers
+    message: `<b>Followers with 90k and above followers</b>:\n\n${followers
       .map(
         (user) =>
           `➡️<a href="https://x.com/${user.username}">@${user.username}</a> - followers:${user.followersCount}`,
